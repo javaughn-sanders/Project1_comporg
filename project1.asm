e5dcda1715f63491f551a267cb9b4206843ec9a6
javaughn-sanders <javaughnd.sanders@gmail.com>
2018-10-15 22:32:44 -0400

    Update project1.asm
    On the right track now

--- project1.asm
+++ project1.asm
@@ -1,11 +1,17 @@
-.data
-	myinfo:		.ascii "@02842578\n"
-			.ascii	"Sanders, Javaughn"
-	
-	
-.text
 
-  la	$a0, myinfo
-  
-  li $v0, 4
-  syscall
+.text
+  main:
+    li $v0, 11
+    la $a0, 74        # J
+    syscall
+    addi $a0, $a0, 23 # a (97)
+    syscall
+    sub $a0, $a0, 11 # V (86)
+    syscall
+    addi $a0, $a0, 11 # a (97)
+    syscall
+    addi $a0, $a0, 20 # u (117)
+    syscall
+    sub $a0, $a0, 14  # g (103)
+    syscall
+   
