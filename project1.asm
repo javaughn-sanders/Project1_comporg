90e14d861c2a168c592f712d281906601886484d
javaughn-sanders <javaughnd.sanders@gmail.com>
2018-10-22 11:11:28 -0400

    finished

--- project1.asm
+++ project1.asm
@@ -1,35 +1,64 @@
 
 .text
   main:
-    li $v0, 11
-    la $a0, 83        # S
+    li $v0, 11		   #Print chars
+    la $a0, 64             # @ (64)
     syscall
-    addi $a0, $a0, 14 # a (97)
+    sub $v0, $v0, 10       # Print integers
+    sub $a0, $a0, 64       # 0 (64-64)
     syscall
-    addi $a0, $a0, 13 # n (110)
+    addi $a0, $a0, 2       # 2 (0+2)
     syscall
-    sub $a0, $a0, 10 # d (100)
+    addi $a0, $a0, 6       # 8 (2+6)
     syscall
-    addi $a0, $a0, 1 # e (101)
+    sub $a0, $a0, 4        # 4 (8-4)
     syscall
-    addi $a0, $a0, 13  # r (114)
+    sub $a0, $a0, 2        # 2 (4-2)
     syscall
-    addi $a0, $a0, 1 # s (115)
+    addi $a0, $a0, 3       # 5 (2+3)
     syscall
-    sub $a0, $a0, 71 # , (44)
+    addi $a0, $a0, 2       # 7 (5+2)
     syscall
-    sub $a0, $a0, 12 # space 32
+    addi $a0, $a0, 2       # 9 (7+2)
     syscall
-    addi $a0, $a0, 42 # J (74)
+    
+    addi $v0,$v0, 10	   #Print chars
+    
     syscall
-    addi $a0, $a0, 23  # a (97)
+    addi $a0, $a0, 1	   # LF (10)
     syscall
-    sub $a0, $a0, 11 # V (86)
+    addi $a0, $a0, 73      # S (10 + 73) (83) 
     syscall
-    addi $a0, $a0, 11 # a (97)
+    addi $a0, $a0, 14      # a (83 + 14)  (97)
     syscall
-    addi $a0, $a0, 20 # u (117)
+    addi $a0, $a0, 13      # n (97 + 13)(110)
     syscall
-    sub $a0, $a0, 14  # g (103)
+    sub $a0, $a0, 10       # d (110 - 10)(100)
+    syscall 
+    addi $a0, $a0, 1       # e (100 + 1)(101)
+    syscall
+    addi $a0, $a0, 13      # r (100 + 13)(114)
+    syscall
+    addi $a0, $a0, 1       # s (114 + 1)(115)
+    syscall
+    sub $a0, $a0, 71       # , (115 - 71)(44)
+    syscall
+    sub $a0, $a0, 12       # space (44 - 12)(32)
+    syscall
+    addi $a0, $a0, 42      # J (32 + 42)(74)
+    syscall
+    addi $a0, $a0, 23      # a (74 + 23)(97)
+    syscall
+    sub $a0, $a0, 11       # V (97 - 11)(86)
+    syscall
+    addi $a0, $a0, 11      # a (86 + 11)(97)
+    syscall
+    addi $a0, $a0, 20      # u (97 + 20)(117)
+    syscall
+    sub $a0, $a0, 14       # g (117 + 14)(103)
+    syscall
+    addi $a0, $a0, 1       # h (103 + 1)(104)
+    syscall
+    addi $a0, $a0, 6       # n (104 + 110)(110)
     syscall
    
